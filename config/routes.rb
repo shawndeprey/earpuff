2742ed5ee51348fa989686917d0394c7425e62f6
Shawn Deprey <shawndeprey@gmail.com>
2013-08-11 02:10:58 -0700

    Added contact us page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Earpuff::Application.routes.draw do
   post 'charges/create' => 'charge#create'
   get 'admin/approve_users' => 'admin#approve_index', as: :approve_users
   get 'admin/approve_user/:id' => 'admin#approve_user'
+  get 'contact' => 'default#contact'
+
   devise_for :users
   root 'default#index'
 end
