0f048437c8e356b4d7c3c89c85e0d70ab153f544
Justin Hammond <justin.35f@gmail.com>
2013-08-10 19:31:02 -0700

    Rails integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+	# Bootstrap Guide Resource: http://railsapps.github.io/twitter-bootstrap-rails.html
+
+	# Simple Form for Twitter Bootstrap
+	# Inside your views, use the 'simple_form_for' with one of the Bootstrap form
+  # classes, '.form-horizontal', '.form-inline', '.form-search' or
+  # '.form-vertical', as the following:
+  #
+  #   = simple_form_for(@user, html: {class: 'form-horizontal' }) do |form|
+
+	# General Helpers
+	LOGO = "/images/earpuff-logo.png"
+
+	def display_base_errors resource
+    return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
+    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-block">
+      <button type="button" class="close" data-dismiss="alert">&#215;</button>
+      #{messages}
+    </div>
+    HTML
+    html.html_safe
+  end
+
 end
